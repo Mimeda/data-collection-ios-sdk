1ef14ffe2c0e253a375e4b8168d75bd375978150
Sezgin Çiftçi <88513574+SezginCiftci@users.noreply.github.com>
2024-08-10 21:32:45 +0300

    ADD: Package Basic Configurations.

---
+++ Sources/MlinkSDK/MlinkSDK.swift
@@ -0,0 +1,14 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+
+public class Logger {
+    
+    static public func printLog(_ text: String?) {
+        if let text {
+            print("MlinkSDK: 🚀🚀🚀🚀 \(text) 🚀🚀🚀🚀")
+        }
+    }
+    
+}
